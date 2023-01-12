be8cbfa62079cf6a7dc826cf8b220f7ec09a8950
Albotrosi <sanekprogamer13@gmail.com>
2023-01-12 14:02:54 +0200

    home work

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let elCat = document.querySelector ('.test');
+console.log('elCat:',elCat);
+elCat.addEventListener('mouseenter',()=>{
+    //BEGIN!
+    console.log('mouseenter!!!');
+    document.write('<b>куку</b>')
+});
