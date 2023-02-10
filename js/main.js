e04cc5a27ddf473ffdf3ddecae3a1ba9d94957cf
Albotrosi <sanekprogamer13@gmail.com>
2023-02-10 12:21:53 +0200

    home work

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 document.forms[0].oninput = () => {
-    // ищем элемент <select>
-    const select = document.querySelector('select')
-    // ищем элемент <div class="fruits">
-    const fruits = document.querySelector('.fruits');
-    // ищем элементы <option>
-    const options = select.querySelectorAll('option');
-    // собираем значения
-    const values = [...options].map(option => option.value);
-    // Удаляем все классы из списка классов fruits
-    for (const item of values) {
-      fruits.classList.remove(item);
-    }
-    // Добавляем класс fruits равную значению value 
-    //            из выбранного элемента в <select>
-    fruits.classList.add(select.value);
+  // ищем элемент <select>
+  const select = document.querySelector('select')
+  // ищем элемент <div class="fruits">
+  const fruits = document.querySelector('.fruits');
+  // ищем элементы <option>
+  const options = select.querySelectorAll('option');
+  // собираем значения
+  const values = [...options].map(option => option.value);
+  // Удаляем все классы из списка классов fruits
+  for (const item of values) {
+    fruits.classList.remove(item);
   }
+  // Добавляем класс fruits равную значению value 
+  //            из выбранного элемента в <select>
+  fruits.classList.add(select.value);
+}
+document.querySelector('button').onclick = myClick;
+function myClick() {
+  let a = document.querySelector ('.i-1').value;
+  console.log(a);
+  document.querySelector('.out').innerHTML = a ;
+}
