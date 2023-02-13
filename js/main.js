5cdb35926dd3d615d459ebec313f6fc60b38c286
Albotrosi <sanekprogamer13@gmail.com>
2023-02-13 17:03:38 +0200

    home work

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
-document.forms[0].oninput = () => {
-  // ищем элемент <select>
-  const select = document.querySelector('select')
-  // ищем элемент <div class="fruits">
-  const fruits = document.querySelector('.fruits');
-  // ищем элементы <option>
-  const options = select.querySelectorAll('option');
-  // собираем значения
-  const values = [...options].map(option => option.value);
-  // Удаляем все классы из списка классов fruits
-  for (const item of values) {
-    fruits.classList.remove(item);
-  }
-  // Добавляем класс fruits равную значению value 
-  //            из выбранного элемента в <select>
-  fruits.classList.add(select.value);
-}
-document.querySelector('button').onclick = myClick;
-function myClick() {
-  let a = document.querySelector ('.i-1').value;
-  console.log(a);
-  document.querySelector('.out').innerHTML = a ;
-}
+let lastActivate = 0;
+let pushed = false;
+document.getElementById('go').addEventListener('click', function() {
+  if (pushed)
+    return;
+  pushed = true;
+  const curDate = new Date();
+  const wait = Math.max(
+    60000 - (new Date().getTime() - lastActivate),
+    5000
+  );
+  setTimeout(
+    function() {
+      document.querySelector('#main .active').classList.remove('active');
+      document.querySelector('#main .yellow').classList.add('active');
+      setTimeout(
+        function() {
+          document.querySelector('#main .active').classList.remove('active');
+          document.querySelector('#main .red').classList.add('active');
+          document.querySelector('#second .active').classList.remove('active');
+          document.querySelector('#second .green').classList.add('active');
+          setTimeout(
+            function() {
+              document.querySelector('#main .active').classList.remove('active');
+              document.querySelector('#main .green').classList.add('active');
+              document.querySelector('#second .active').classList.remove('active');
+              document.querySelector('#second .red').classList.add('active');
+              lastActivate = new Date().getTime();
+              pushed = false;
+            }, 15000
+          );
+        }, 5000
+      );
+    }, wait
+  );
+});
