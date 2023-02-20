a026826474d6a5abaa38c06fb5d2cf1e20c05060
Albotrosi <sanekprogamer13@gmail.com>
2023-02-20 15:25:03 +0200

    home work

--- js/main.js
+++ js/main.js
@@ -1,36 +1,26 @@
-let lastActivate = 0;
-let pushed = false;
-document.getElementById('go').addEventListener('click', function() {
-  if (pushed)
-    return;
-  pushed = true;
-  const curDate = new Date();
-  const wait = Math.max(
-    60000 - (new Date().getTime() - lastActivate),
-    5000
-  );
-  setTimeout(
-    function() {
-      document.querySelector('#main .active').classList.remove('active');
-      document.querySelector('#main .yellow').classList.add('active');
-      setTimeout(
-        function() {
-          document.querySelector('#main .active').classList.remove('active');
-          document.querySelector('#main .red').classList.add('active');
-          document.querySelector('#second .active').classList.remove('active');
-          document.querySelector('#second .green').classList.add('active');
-          setTimeout(
-            function() {
-              document.querySelector('#main .active').classList.remove('active');
-              document.querySelector('#main .green').classList.add('active');
-              document.querySelector('#second .active').classList.remove('active');
-              document.querySelector('#second .red').classList.add('active');
-              lastActivate = new Date().getTime();
-              pushed = false;
-            }, 15000
-          );
-        }, 5000
-      );
-    }, wait
-  );
+first("button").addEventListener("click", function() {
+  let val = first(".myInput").value.trim();
+  if (!val) return; // ничего не ввели? Прервать.
+  
+  first(".textOutput").insertAdjacentHTML("beforeEnd", prepare(val));
+  first(".myInput").value = '';
 });
+
+function prepare(text) {
+  return "<li>" + text.split(/\s+/).map(wrap_span).join(" ") + "</li>";
+  // в регулярном выражении, \s+ =один или много пробелов подряд
+  
+  /***/
+  function wrap_span(word) {    
+    let color = /[!@#$&*%]/.test(word) ? "red" : word.length > 3 ? "green" : "";
+
+    return `<span style="color: ${ color }">${ word }</span>`;
+  }
+}
+function first(str) {
+  return document.querySelector(str);
+}
+document.getElementById('reset').onclick = function() {
+  document.forms.my.reset();
+  location.reload();
+}
