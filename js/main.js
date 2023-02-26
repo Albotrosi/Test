8b3ebe655e8d0062b9edf004b94491a12d0eee95
Albotrosi <sanekprogamer13@gmail.com>
2023-02-26 23:23:11 +0200

    home work

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
-let push = document.querySelector(".clickButton")
-let newtext = document.querySelector(".textOutput")
-push.addEventListener('click',function(){
-  let a = document.querySelector(".myInput").value;
-  document.querySelector(".myInput").value = '';
-  document.querySelector(".textOutput").innerHTML += prepare(a);
-}) 
-prepare = function (text){
-  return text
-    .split(' ')
-    .map(word => {
-      if (word.length === 0) {
-        return word;
-      }
-      let color = '';
-      if (word.length > 3) {
-        color = 'green';
-      }
-      if (word.match(/[!@#$&*%]/)) {
-        color = 'red';
-      }
-      return `<ul style="color: ${color}">${word}</ul>`;
-    }).join(' ');
-};
-let reset = document.querySelector(".reset");
-reset.addEventListener("click",function(){
-  document.location.reload();
+var img = document.querySelectorAll('.click'),
+  backPop = document.getElementById('backPop'),
+    popUp = document.getElementById('popUp'),
+    bigImg= document.getElementById('bigImg');
+[].forEach.call(img, function(elem) {
+  var src = elem.getAttribute('src');
+  elem.onclick = function() { 
+    bigImg.setAttribute('src',src);
+    backPop.style.display = "block";
+  }
 });
+backPop.onclick = function(elem){
+  if(!(elem.target.closest("#popUp"))){
+      backPop.style.display = 'none';
+     }    
+}
