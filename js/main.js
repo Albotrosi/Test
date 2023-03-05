20ad9bedbb642850e9ec5e81c82fbdf173d5b592
Albotrosi <sanekprogamer13@gmail.com>
2023-03-05 15:26:38 +0200

    home work

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-let img = document.querySelectorAll('.click'),
-  backPop = document.getElementById('backPop'),
-    popUp = document.getElementById('popUp'),
-    bigImg= document.getElementById('bigImg');
-[].forEach.call(img, function(elem) {
-  let src = elem.getAttribute('src');
-  elem.addEventListener ('click', function() { 
-    bigImg.setAttribute('src',src);
-    backPop.style.display = "block";
-  })
+push = document.querySelector(".clickButton")
+newtext = document.querySelector(".textOutput")
+push.addEventListener('click',function(){
+  let a = document.querySelector(".myInput").value;
+  let b = document.querySelector(".myInput-2").value;
+  let v = document.querySelector(".myInput-3").value;
+  document.querySelector(".textOutput").innerHTML += prepare(a,b,v);
+  document.querySelector(".textOutput-2").innerHTML += prepare(b);
+  document.querySelector(".textOutput-3").innerHTML += prepare(v);
+  document.querySelector(".myInput").value = '';
+}) 
+prepare = function (text){
+  return text
+    .split(' ')
+    .map(word => {
+      if (word.length === 0) {
+        return word;
+      }
+      let color = '';
+      if (word.length > 3) {
+        color = 'green';
+      }
+      if (word.match(/[!@#$&*%]/)) {
+        color = 'red';
+      }
+      return `<ul style="color: ${color}">${word}</ul>`;
+    }).join(' ');
+};
+reset = document.querySelector(".reset");
+reset.addEventListener("click",function(){
+  document.location.reload();
 });
-backPop.addEventListener ('click', function (elem){
-  if(!(elem.target.closest("#popUp"))){
-      backPop.style.display = 'none';
-     }    
-})
