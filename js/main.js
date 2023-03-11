a60c4c4538de0c47dfcf01e929da4174bab62dbc
Albotrosi <sanekprogamer13@gmail.com>
2023-03-11 11:21:37 +0200

    home work

--- js/main.js
+++ js/main.js
@@ -1,32 +1,14 @@
-push = document.querySelector(".clickButton")
-newtext = document.querySelector(".textOutput")
-push.addEventListener('click',function(){
-  let a = document.querySelector(".myInput").value;
-  let b = document.querySelector(".myInput-2").value;
-  let v = document.querySelector(".myInput-3").value;
-  document.querySelector(".textOutput").innerHTML += prepare(a,b,v);
-  document.querySelector(".textOutput-2").innerHTML += prepare(b);
-  document.querySelector(".textOutput-3").innerHTML += prepare(v);
-  document.querySelector(".myInput").value = '';
-}) 
-prepare = function (text){
-  return text
-    .split(' ')
-    .map(word => {
-      if (word.length === 0) {
-        return word;
-      }
-      let color = '';
-      if (word.length > 3) {
-        color = 'green';
-      }
-      if (word.match(/[!@#$&*%]/)) {
-        color = 'red';
-      }
-      return `<ul style="color: ${color}">${word}</ul>`;
-    }).join(' ');
+const random = (min,max) => {
+  const rand = min + Math.random() * (max - min + 1);
+  return Math.floor(rand);
 };
-reset = document.querySelector(".reset");
-reset.addEventListener("click",function(){
-  document.location.reload();
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('mouseenter', () =>{
+  btn.style.left = `${random(0,90)}%`;
+  btn.style.top = `${random(0,90)}%`;
 });
+
+btn.addEventListener('click',() => {
+  alert('молодець! давайте зіграемо ще разок?')
+})
