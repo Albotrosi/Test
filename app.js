7f79f2471d81ef915634d1f4f805f39980a58cd1
Albotrosi <sanekprogamer13@gmail.com>
2023-04-14 12:16:13 +0300

    home work

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-let http = require('http');
-let server = http.createServer( function(req, res){
-    // req . url и путь к файлу у вас может быть другим
-    if( req . res === '/index.html'){// отдаем html
-      res.writeHead(200, {
-          'Content-Type':'index.html; charset=utf-8',
-      });
-      let myReadShort = fs.createReadStream(__dirname + '/H.C.html','utf-8');
-      myReadShort.pipe(res);
-    } else if( req . url === '/style.css'){// отдаем css
-      res.writeHead(200, {
-          'Content-Type':'text/css; charset=utf-8',
-      });
-      let myReadShort = fs.createReadStream(__dirname + '/style.css','utf-8');
-      myReadShort.pipe(res);
-    } else { // отдаем ошибку
-      res.writeHead(404, {
-          'Content-Length':'0',
-      });
-    }
-  });
-console.log('aaaaa');
-server.listen(3000)
+const express = require('express');
+
+const app = express();
+
+const FOLDER = `${process.cwd()}/public`;
+app.use(express.static(FOLDER));
+
+const port = 3000;
+const hostname = 'localhost';
+app.listen(port, hostname, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
